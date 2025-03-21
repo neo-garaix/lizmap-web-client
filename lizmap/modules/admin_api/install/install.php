4047af6f2be6dee4624eb287f5ce616fc9050276
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2025-03-21 13:23:34 +0100

    REMOVE: unnecessary lines

--- lizmap/modules/admin_api/install/install.php
+++ lizmap/modules/admin_api/install/install.php
@@ -1,22 +1,17 @@
 <?php
+
+use Jelix\Installer\Module\API\InstallHelpers;
+use Jelix\Installer\Module\Installer;
+
 /**
-* @package   lizmap
-* @subpackage admin_api
-* @author    3liz.com
-* @copyright 2011-2025 3Liz
-* @link      https://3liz.com
-* @license   https://www.mozilla.org/MPL/ Mozilla Public Licence
-*/
-
-
-class admin_apiModuleInstaller extends \Jelix\Installer\Module\Installer {
-
-    function install(\Jelix\Installer\Module\API\InstallHelpers $helpers) {
-        //$helpers->database()->execSQLScript('sql/install');
-
-        /*
-        jAcl2DbManager::createRight('my.right', 'admin_api~acl.my.right', 'right.group.id');
-        jAcl2DbManager::addRight('admins', 'my.right'); // for admin group
-        */
-    }
+ * @author    3liz.com
+ * @copyright 2011-2025 3Liz
+ *
+ * @see      https://3liz.com
+ *
+ * @license   https://www.mozilla.org/MPL/ Mozilla Public Licence
+ */
+class admin_apiModuleInstaller extends Installer
+{
+    public function install(InstallHelpers $helpers) {}
 }
